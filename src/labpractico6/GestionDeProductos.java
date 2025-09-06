d71f4f25763e82217f3585243a8b57321732e0ba
franconicolasilva <francounvime@gmail.com>
2025-09-06 15:05:49 -0300

    UPDATE @Franco
    Regresamos

--- src/labpractico6/GestionDeProductos.java
+++ src/labpractico6/GestionDeProductos.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JInternalFrame.java to edit this template
  */
-package Backup;
+package labpractico6;
 
 import java.util.Map;
 import java.util.TreeMap;
